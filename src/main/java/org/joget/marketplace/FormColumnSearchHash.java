6bdab53581c79b9f4c5edc14a2a18ef573262259
miaoxng <miaoxuan.ng@joget.com>
2023-12-28 15:13:45 +0800

    Modified: Update version for release

--- src/main/java/org/joget/marketplace/FormColumnSearchHash.java
+++ src/main/java/org/joget/marketplace/FormColumnSearchHash.java
@@ -25,7 +25,7 @@ public class FormColumnSearchHash extends DefaultHashVariablePlugin {
 
     @Override
     public String getVersion() {
-        return "8.0.1";
+        return "8.0.2";
     }
 
     @Override
